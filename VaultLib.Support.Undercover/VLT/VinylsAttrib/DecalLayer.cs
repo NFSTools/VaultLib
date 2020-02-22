1820ddb816c17b123b09307b32626c5a30e0ea89
leo <coderleo42+leocodes@gmail.com>
2020-02-22 14:31:52 -0500

    implement 1 missing Undercover type that isn't actually used in the stock database

---
+++ VaultLib.Support.Undercover/VLT/VinylsAttrib/DecalLayer.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using CoreLibraries.IO;
+using VaultLib.Core;
+using VaultLib.Core.Data;
+using VaultLib.Core.Types;
+
+namespace VaultLib.Support.Undercover.VLT.VinylsAttrib
+{
+    [VLTTypeInfo("VinylsAttrib::DecalLayer")]
+    public class DecalLayer : VLTBaseType
+    {
+        public DecalLayer(VltClass @class, VltClassField field, VltCollection collection) : base(@class, field, collection)
+        {
+        }
+
+        public DecalLayer(VltClass @class, VltClassField field) : base(@class, field)
+        {
+        }
+
+        public uint PartNameHash { get; set; }
+        public bool Mirrored { get; set; }
+        public VinylTransform Transform { get; set; }
+        public VinylColor Color { get; set; }
+
+        public override void Read(Vault vault, BinaryReader br)
+        {
+            Transform = new VinylTransform(Class, Field, Collection);
+            Color = new VinylColor(Class, Field, Collection);
+            PartNameHash = br.ReadUInt32();
+            Mirrored = br.ReadBoolean();
+            br.AlignReader(4);
+            Transform.Read(vault, br);
+            Color.Read(vault, br);
+        }
+
+        public override void Write(Vault vault, BinaryWriter bw)
+        {
+            bw.Write(PartNameHash);
+            bw.Write(Mirrored);
+            bw.AlignWriter(4);
+            Transform.Write(vault, bw);
+            Color.Write(vault, bw);
+        }
+    }
+}
