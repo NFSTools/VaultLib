efae215900211073b589bd52feca57b3b1abbb07
leo <coderleo42+leocodes@gmail.com>
2020-02-28 20:37:45 -0500

    VaultLib.Support.World: GameCore.LocalizationHash is now a primitive type

--- VaultLib.Support.World/VLT/GameCore/LocalizationHash.cs
+++ VaultLib.Support.World/VLT/GameCore/LocalizationHash.cs
@@ -1,25 +1,12 @@
-using System.IO;
-using VaultLib.Core;
 using VaultLib.Core.Data;
 using VaultLib.Core.Types;
+using VaultLib.Core.Types.EA.Reflection;
 
 namespace VaultLib.Support.World.VLT.GameCore
 {
     [VLTTypeInfo("GameCore::LocalizationHash")]
-    public class LocalizationHash : VLTBaseType
+    public class LocalizationHash : UInt32
     {
-        public uint Hash { get; set; }
-
-        public override void Read(Vault vault, BinaryReader br)
-        {
-            Hash = br.ReadUInt32();
-        }
-
-        public override void Write(Vault vault, BinaryWriter bw)
-        {
-            bw.Write(Hash);
-        }
-
         public LocalizationHash(VltClass @class, VltClassField field, VltCollection collection) : base(@class, field, collection)
         {
         }
