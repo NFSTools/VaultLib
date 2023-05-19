524055b2a03ee9a9cec0e49f0eb0f4ef6ab4a10b
leo <coderleo42+leocodes@gmail.com>
2023-05-19 19:57:59 -0400

    fix(game-world): correct implementation of Attrib::Query::Static_Inorder_N_to_1

--- VaultLib.Support.World/VLT/Attrib/Query/Static_Inorder_N_to_1.cs
+++ VaultLib.Support.World/VLT/Attrib/Query/Static_Inorder_N_to_1.cs
@@ -2,12 +2,9 @@
 // 
 // Created: 09/27/2019 @ 5:45 PM.
 
-using CoreLibraries.IO;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using CoreLibraries.IO;
 using VaultLib.Core;
 using VaultLib.Core.Data;
 using VaultLib.Core.Hashing;
@@ -20,142 +17,121 @@ namespace VaultLib.Support.World.VLT.Attrib.Query
         "Attrib::Query::Static_Inorder_N_to_1<Attrib::Query::Typespace<Attrib::Key,Attrib::Key,EA::Reflection::UInt32> >")]
     public class Static_Inorder_N_to_1 : VLTBaseType, IPointerObject
     {
-        public long ElementsPointer { get; private set; }
-        public long ElementsDest { get; private set; }
-        public long TreePointer { get; private set; }
-        public long TreeDest { get; private set; }
-        public long EndPointer { get; private set; }
-        public long EndDest { get; private set; }
+        private long _leavesDst;
+        private long _leavesPointer;
+        private long _nodesDst;
+        private long _nodesPointer;
+        private uint _numRoots;
 
-        public override void Read(Vault vault, BinaryReader br)
-        {
-            uint numElements = br.ReadUInt32();
+        private long _numRootsDst;
+        private long _rootsDst;
+        private long _rootsPointer;
 
-            List<uint> elements = new List<uint>();
-            uint elementsPointer = br.ReadUInt32();
-            uint treePointer = br.ReadUInt32();
-            uint endPointer = br.ReadUInt32();
-
-            for (int i = 0; i < numElements; i++)
-            {
-                if (br.BaseStream.Position < elementsPointer || br.BaseStream.Position > treePointer)
-                {
-                    throw new Exception("Static_Inorder read failed");
-                }
-
-                elements.Add(br.ReadUInt32());
-
-                //Debug.WriteLine("\tArray element: {0:X} ({1})", elements[i], HashManager.ResolveVLT(elements[i]));
-            }
-
-            uint lastIndex = 0u;
-
-            for (int i = 0; i < numElements; i++)
-            {
-                uint index = br.ReadUInt32();
-                uint childrenCount = br.ReadUInt32();
-
-                Debug.Assert(index == lastIndex);
-                lastIndex += childrenCount;
-                //Debug.WriteLine("\tTree element: I={0} C={1}", index, childrenCount);
-            }
-        }
-
-        public override void Write(Vault vault, BinaryWriter bw)
-        {
-            ElementsPointer = 0;
-            TreePointer = 0;
-            EndPointer = 0;
-
-            // Obtain the full list of collections
-            List<VltCollection> allCollections = vault.SaveContext.Collections.Where(c => c.Class.Name == Class.Name).ToList();
-            Dictionary<VltCollection, uint> keys = allCollections.ToDictionary(c => c, c => VLT32Hasher.Hash(c.Name));
-
-            // Group list by parent
-            Dictionary<uint, List<VltCollection>> groupedByParent = allCollections.GroupBy(c => c.Parent != null ? keys[c.Parent] : 0)
-                .ToDictionary(g => g.Key, g => g.ToList());
-
-
-            // Filter list to collections with children
-            List<VltCollection> withChildren = allCollections.Where(c => groupedByParent.ContainsKey(keys[c])).OrderBy(c => keys[c]).ToList();
-
-            // Get list of top-level (no parent) collections
-            List<VltCollection> topLevel = allCollections.Where(c => c.Parent == null).ToList();
-
-            List<uint> arrayElements = new List<uint>
-            {
-                0 // 0 represents the class root
-            };
-
-            // Collections with children are in the array and tree
-            foreach (VltCollection c in withChildren)
-            {
-                arrayElements.Add(keys[c]);
-            }
-
-            Dictionary<int, int> tree = new Dictionary<int, int>
-            {
-                { 0, topLevel.Count } // class root
-            };
-
-            int treeIdx = topLevel.Count;
-            int arrIdx = 1;
-
-            for (; arrIdx < arrayElements.Count; arrIdx++)
-            {
-                tree.Add(treeIdx, groupedByParent[arrayElements[arrIdx]].Count);
-
-                treeIdx += tree[treeIdx];
-            }
-
-            Debug.Assert(tree.Count == arrayElements.Count);
-
-            bw.Write(arrayElements.Count);
-            ElementsPointer = bw.WritePointer();
-            TreePointer = bw.WritePointer();
-            EndPointer = bw.WritePointer();
-
-            ElementsDest = bw.BaseStream.Position;
-
-            foreach (uint arrayElement in arrayElements)
-            {
-                bw.Write(arrayElement);
-            }
-
-            TreeDest = bw.BaseStream.Position;
-
-            foreach (KeyValuePair<int, int> i in tree)
-            {
-                bw.Write(i.Key);
-                bw.Write(i.Value);
-            }
-
-            EndDest = bw.BaseStream.Position;
-        }
-
-        public void ReadPointerData(Vault vault, BinaryReader br)
-        {
-            // do nothing
-        }
-
-        public void WritePointerData(Vault vault, BinaryWriter bw)
-        {
-            // do nothing as well
-        }
-
-        public void AddPointers(Vault vault)
-        {
-            vault.SaveContext.AddPointer(ElementsPointer, ElementsDest, false);
-            vault.SaveContext.AddPointer(TreePointer, TreeDest, false);
-            vault.SaveContext.AddPointer(EndPointer, EndDest, false);
-        }
-
-        public Static_Inorder_N_to_1(VltClass @class, VltClassField field, VltCollection collection) : base(@class, field, collection)
+        public Static_Inorder_N_to_1(VltClass @class, VltClassField field, VltCollection collection) : base(@class,
+            field, collection)
         {
         }
 
         public Static_Inorder_N_to_1(VltClass @class, VltClassField field) : base(@class, field)
         {
+        }
+
+        public void ReadPointerData(Vault vault, BinaryReader br)
+        {
+            // This code only exists for testing
+            // br.BaseStream.Position = _rootsPointer;
+            //
+            // // roots and nodes are sorted by key
+            // var roots = new uint[_numRoots];
+            //
+            // for (var i = 0; i < _numRoots; i++)
+            // {
+            //     roots[i] = br.ReadUInt32();
+            // }
+            //
+            // br.BaseStream.Position = _nodesPointer;
+            //
+            // var nodes = new (uint Index, uint NumChildren)[_numRoots];
+            //
+            // for (var i = 0; i < _numRoots; i++)
+            // {
+            //     nodes[i].Index = br.ReadUInt32();
+            //     nodes[i].NumChildren = br.ReadUInt32();
+            // }
+            //
+            // br.BaseStream.Position = _leavesPointer;
+            //
+            // // individual child lists are sorted by collection name?
+            // // it probably doesn't actually matter but that's what it looks like
+            // // the list of lists is sorted by key, i.e.,
+            // // childLists[i] corresponds with nodes[i]
+            // // corresponds with roots[i]
+            // var childLists = new uint[_numRoots][];
+            //
+            // for (var i = 0; i < _numRoots; i++)
+            // {
+            //     childLists[i] = new uint[nodes[i].NumChildren];
+            //
+            //     for (var j = 0; j < childLists[i].Length; j++)
+            //     {
+            //         childLists[i][j] = br.ReadUInt32();
+            //     }
+            // }
+            //
+            // // Debugger.Break();
+        }
+
+        public void WritePointerData(Vault vault, BinaryWriter bw)
+        {
+            var groupedCollections = vault.Database.RowManager.EnumerateFlattenedCollections(Class.Name)
+                .GroupBy(c => VLT32Hasher.Hash(c.Parent?.Name));
+            var sortedGroups = groupedCollections.OrderBy(g => g.Key).ToList();
+
+            var curPos = bw.BaseStream.Position;
+            bw.BaseStream.Position = _numRootsDst;
+            bw.Write(sortedGroups.Count);
+            bw.BaseStream.Position = curPos;
+
+            _rootsDst = bw.BaseStream.Position;
+            foreach (var group in sortedGroups) bw.Write(group.Key);
+
+            _nodesDst = bw.BaseStream.Position;
+            var leafStartIndex = 0;
+            foreach (var group in sortedGroups)
+            {
+                bw.Write(leafStartIndex);
+                var numChildren = group.Count();
+                bw.Write(numChildren);
+                leafStartIndex += numChildren;
+            }
+
+            _leavesDst = bw.BaseStream.Position;
+            foreach (var collection in sortedGroups.SelectMany(group => group.OrderBy(c => c.Name)))
+                bw.Write(VLT32Hasher.Hash(collection.Name));
+        }
+
+        public void AddPointers(Vault vault)
+        {
+            vault.SaveContext.AddPointer(_rootsPointer, _rootsDst, false);
+            vault.SaveContext.AddPointer(_nodesPointer, _nodesDst, false);
+            vault.SaveContext.AddPointer(_leavesPointer, _leavesDst, false);
+        }
+
+        public override void Read(Vault vault, BinaryReader br)
+        {
+            _numRoots = br.ReadUInt32();
+            _rootsPointer = br.ReadPointer();
+            _nodesPointer = br.ReadPointer();
+            _leavesPointer = br.ReadPointer();
+        }
+
+        public override void Write(Vault vault, BinaryWriter bw)
+        {
+            _numRootsDst = bw.BaseStream.Position;
+            bw.Write(0xAAAAAAAA);
+            _rootsPointer = bw.WritePointer();
+            _nodesPointer = bw.WritePointer();
+            _leavesPointer = bw.WritePointer();
         }
     }
 }
