9443c6b338c5c09d776cc6a84af9664e2a9e5b27
leo <coderleo42+leocodes@gmail.com>
2025-01-29 11:12:26 -0500

    fix(carbon): remove mTireCondition from TireEffectRecord
    It seems to be taking the place of the third field of RefSpec32.

--- VaultLib.Support.Carbon/VLT/TireEffectRecord.cs
+++ VaultLib.Support.Carbon/VLT/TireEffectRecord.cs
@@ -1,32 +1,29 @@
 ﻿using System.IO;
-using CoreLibraries.IO;
 using VaultLib.Core;
 using VaultLib.Core.DataInterfaces;
 using VaultLib.Core.Types;
 using VaultLib.Core.Types.Attrib;
-using VaultLib.Frameworks.Speed.VLT;
 
 namespace VaultLib.Support.Carbon.VLT;
 
 [VltTypeInfo(nameof(TireEffectRecord))]
-public class TireEffectRecord: VltBaseType<Key32>
+public class TireEffectRecord : VltBaseType<Key32>
 {
-    public TireCondition mTireCondition { get; set; }
-    public RefSpecPacked32 mEmitter { get; set; } = new();
+    public RefSpec32 mEmitter { get; set; } = new();
     public float mMinSpeed { get; set; }
     public float mMaxSpeed { get; set; }
 
-    public override void Read(VaultReadContext<Key32> context, FieldReadWriteContext<Key32> fieldContext, BinaryReader br)
+    public override void Read(VaultReadContext<Key32> context, FieldReadWriteContext<Key32> fieldContext,
+        BinaryReader br)
     {
-        mTireCondition = br.ReadEnum<TireCondition>();
         mEmitter.Read(context, fieldContext, br);
         mMinSpeed = br.ReadSingle();
         mMaxSpeed = br.ReadSingle();
     }
 
-    public override void Write(VaultWriteContext<Key32> context, FieldReadWriteContext<Key32> fieldContext, BinaryWriter bw)
+    public override void Write(VaultWriteContext<Key32> context, FieldReadWriteContext<Key32> fieldContext,
+        BinaryWriter bw)
     {
-        bw.WriteEnum(mTireCondition);
         mEmitter.Write(context, fieldContext, bw);
         bw.Write(mMinSpeed);
         bw.Write(mMaxSpeed);
