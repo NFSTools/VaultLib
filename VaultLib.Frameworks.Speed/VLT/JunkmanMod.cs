23941cdcd3dcadd12cb394f9d4086a4bdc1a409d
leo <coderleo42+leocodes@gmail.com>
2025-01-29 11:05:56 -0500

    fix(speed): JunkmanMod key fields now use Key32

--- VaultLib.Frameworks.Speed/VLT/JunkmanMod.cs
+++ VaultLib.Frameworks.Speed/VLT/JunkmanMod.cs
@@ -2,15 +2,36 @@
 // 
 // Created: 09/29/2019 @ 11:38 AM.
 
+using System.IO;
+using VaultLib.Core;
+using VaultLib.Core.DataInterfaces;
 using VaultLib.Core.Types;
 
 namespace VaultLib.Frameworks.Speed.VLT;
 
 [VltTypeInfo(nameof(JunkmanMod))]
-public struct JunkmanMod
+public class JunkmanMod : VltBaseType<Key32>
 {
-    public uint ClassKey;
-    public uint DefinitionKey;
-    public float ScaleF;
-    public float ScaleR;
+    public Key32 ClassKey { get; set; }
+    public Key32 DefinitionKey { get; set; }
+    public float ScaleF { get; set; }
+    public float ScaleR { get; set; }
+
+    public override void Read(VaultReadContext<Key32> context, FieldReadWriteContext<Key32> fieldContext,
+        BinaryReader br)
+    {
+        ClassKey = Key32.Read(br);
+        DefinitionKey = Key32.Read(br);
+        ScaleF = br.ReadSingle();
+        ScaleR = br.ReadSingle();
+    }
+
+    public override void Write(VaultWriteContext<Key32> context, FieldReadWriteContext<Key32> fieldContext,
+        BinaryWriter bw)
+    {
+        ClassKey.Write(bw);
+        DefinitionKey.Write(bw);
+        bw.Write(ScaleF);
+        bw.Write(ScaleR);
+    }
 }
