48aae9fd3e5bafd216b4f4bb42981b8b95206690
leo <coderleo42+leocodes@gmail.com>
2025-01-29 23:15:10 -0500

    feat: allow registration of custom export types

--- VaultLib.Core/Chunks/VltExportChunk.cs
+++ VaultLib.Core/Chunks/VltExportChunk.cs
@@ -38,7 +38,7 @@ public class VltExportChunk<TKey> : ChunkBase<TKey> where TKey : struct, IKey<TK
 
             exportEntry.Read(context, br);
 
-            var export = CreateExport(context, exportEntry.Type);
+            var export = context.Database.ExportFactory.CreateExport(exportEntry.Type);
 
             Debug.Assert(export != null);
 
@@ -59,40 +59,5 @@ public class VltExportChunk<TKey> : ChunkBase<TKey> where TKey : struct, IKey<TK
         foreach (var exportEntry in _exports) exportEntry.Write(context, bw);
 
         bw.AlignWriter(0x10);
-    }
-
-    private BaseExport<TKey> CreateExport(VaultReadContext<TKey> context, TKey type)
-    {
-        // TODO: these shouldn't be hardcoded
-
-        switch (type)
-        {
-            case Key64 k64:
-                switch (k64.Hash)
-                {
-                    case 0x2A7895AC4A876152u: // Attrib::ClassLoadData
-                        return context.Database.ExportFactory.BuildClassLoad(null);
-                    case 0xB38846845E9C175u: // Attrib::DatabaseLoadData
-                        return context.Database.ExportFactory.BuildDatabaseLoad();
-                    case 0xAD303B8F42B3307Eu:
-                        return context.Database.ExportFactory.BuildCollectionLoad(null);
-                }
-
-                break;
-            case Key32 k32:
-                switch (k32.Hash)
-                {
-                    case 0x5E970CBCu: // Attrib::ClassLoadData
-                        return context.Database.ExportFactory.BuildClassLoad(null);
-                    case 0xCBBC628Fu: // Attrib::DatabaseLoadData
-                        return context.Database.ExportFactory.BuildDatabaseLoad();
-                    case 0x8E112EB7u:
-                        return context.Database.ExportFactory.BuildCollectionLoad(null);
-                }
-
-                break;
-        }
-
-        return null;
     }
 }
--- VaultLib.Core/Exports/ExportFactory.cs
+++ VaultLib.Core/Exports/ExportFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using VaultLib.Core.Data;
 using VaultLib.Core.DataInterfaces;
 using VaultLib.Core.Structures;
@@ -7,51 +8,17 @@ namespace VaultLib.Core.Exports;
 
 public class ExportFactory<TKey> where TKey : struct, IKey<TKey>
 {
-    // private static readonly Dictionary<string, Func<IExportEntry>> ExportEntryCreatorDictionary =
-    //     new Dictionary<string, Func<IExportEntry>>();
-    //
-    // private static readonly Dictionary<string, Func<IPtrRef>> PtrCreatorDictionary =
-    //     new Dictionary<string, Func<IPtrRef>>();
-    //
-    // private static readonly Dictionary<string, Func<BaseCollectionLoad>> CollectionLoadBuilderDictionary =
-    //     new Dictionary<string, Func<BaseCollectionLoad>>();
-    //
-    // private static readonly Dictionary<string, Func<BaseClassLoad>> ClassLoadBuilderDictionary =
-    //     new Dictionary<string, Func<BaseClassLoad>>();
-    //
-    // private static readonly Dictionary<string, Func<BaseDatabaseLoad>> DatabaseLoadBuilderDictionary =
-    //     new Dictionary<string, Func<BaseDatabaseLoad>>();
-
-    // public static void SetPointerCreator<T>(string game) where T : IPtrRef, new()
-    // {
-    //     PtrCreatorDictionary.Add(game, () => new T());
-    // }
-    //
-    // public static void SetExportEntryCreator<T>(string game) where T : IExportEntry, new()
-    // {
-    //     ExportEntryCreatorDictionary.Add(game, () => new T());
-    // }
-    //
-    // public static void SetCollectionLoadCreator<T>(string game) where T : BaseCollectionLoad, new()
-    // {
-    //     CollectionLoadBuilderDictionary.Add(game, () => new T());
-    // }
-    //
-    // public static void SetClassLoadCreator<T>(string game) where T : BaseClassLoad, new()
-    // {
-    //     ClassLoadBuilderDictionary.Add(game, () => new T());
-    // }
-    //
-    // public static void SetDatabaseLoadCreator<T>(string game) where T : BaseDatabaseLoad, new()
-    // {
-    //     DatabaseLoadBuilderDictionary.Add(game, () => new T());
-    // }
-
     private readonly Func<BaseDatabaseLoad<TKey>> _databaseLoadFactory;
     private readonly Func<BaseClassLoad<TKey>> _classLoadFactory;
     private readonly Func<BaseCollectionLoad<TKey>> _collectionLoadFactory;
     private readonly Func<IExportEntry<TKey>> _exportEntryFactory;
     private readonly Func<IPtrRef<TKey>> _ptrRefFactory;
+
+    private Dictionary<TKey, Func<BaseExport<TKey>>> ExportBuilders { get; } = new();
+
+    private static readonly TKey ClassLoadDataKey = TKey.FromString("Attrib::ClassLoadData");
+    private static readonly TKey CollectionLoadDataKey = TKey.FromString("Attrib::CollectionLoadData");
+    private static readonly TKey DatabaseLoadDataKey = TKey.FromString("Attrib::DatabaseLoadData");
 
     public ExportFactory(Func<BaseDatabaseLoad<TKey>> databaseLoadFactory,
         Func<BaseClassLoad<TKey>> classLoadFactory, Func<BaseCollectionLoad<TKey>> collectionLoadFactory,
@@ -62,6 +29,25 @@ public class ExportFactory<TKey> where TKey : struct, IKey<TKey>
         _collectionLoadFactory = collectionLoadFactory;
         _exportEntryFactory = exportEntryFactory;
         _ptrRefFactory = ptrRefFactory ?? (() => new AttribPtrRef<TKey>());
+
+        ExportBuilders.Add(ClassLoadDataKey, classLoadFactory);
+        ExportBuilders.Add(CollectionLoadDataKey, collectionLoadFactory);
+        ExportBuilders.Add(DatabaseLoadDataKey, databaseLoadFactory);
+    }
+
+    public void RegisterExportType<TExport>(TKey exportType) where TExport : BaseExport<TKey>, new()
+    {
+        ExportBuilders.Add(exportType, () => new TExport());
+    }
+
+    public BaseExport<TKey> CreateExport(TKey exportType)
+    {
+        if (!ExportBuilders.TryGetValue(exportType, out var exportBuilder))
+        {
+            throw new KeyNotFoundException($"No factory found for export type: {exportType}");
+        }
+
+        return exportBuilder();
     }
 
     public BaseCollectionLoad<TKey> BuildCollectionLoad(VltCollection<TKey> collection)
