4590dbd4fcac50191a9760d7b599c0df831d138f
leo <coderleo42+leocodes@gmail.com>
2025-01-31 20:04:25 -0500

    refactor: significantly cleaner implementation of VLT64 hashes

--- VaultLib.Core/Hashing/Vlt64Hasher.cs
+++ VaultLib.Core/Hashing/Vlt64Hasher.cs
@@ -2,147 +2,170 @@
 // 
 // Created: 10/07/2019 @ 7:27 PM.
 
-using System.Globalization;
+using System;
+using System.Buffers.Binary;
 using System.Text;
 
 namespace VaultLib.Core.Hashing;
 
+/// <summary>
+/// C# implementation of "lookup8" by Bob Jenkins.
+/// </summary>
+/// <remarks>Ported from the original C: https://burtleburtle.net/bob/c/lookup8.c</remarks>
 public static class Vlt64Hasher
 {
-    public static ulong Hash(string str, ulong init = 0xABCDEF0011223344, bool returnZeroForEmpty = true)
+    private static void Mix64(ref ulong a, ref ulong b, ref ulong c)
+    {
+        a -= b;
+        a -= c;
+        a ^= (c >> 43);
+        b -= c;
+        b -= a;
+        b ^= (a << 9);
+        c -= a;
+        c -= b;
+        c ^= (b >> 8);
+        a -= b;
+        a -= c;
+        a ^= (c >> 38);
+        b -= c;
+        b -= a;
+        b ^= (a << 23);
+        c -= a;
+        c -= b;
+        c ^= (b >> 5);
+        a -= b;
+        a -= c;
+        a ^= (c >> 35);
+        b -= c;
+        b -= a;
+        b ^= (a << 49);
+        c -= a;
+        c -= b;
+        c ^= (b >> 11);
+        a -= b;
+        a -= c;
+        a ^= (c >> 12);
+        b -= c;
+        b -= a;
+        b ^= (a << 18);
+        c -= a;
+        c -= b;
+        c ^= (b >> 22);
+    }
+
+    /// <summary>
+    /// Produces a 64-bit hash of a string using the lookup8 algorithm.
+    /// </summary>
+    /// <param name="str">The string to hash.</param>
+    /// <param name="initVal">The seed for the hash.</param>
+    /// <param name="returnZeroForEmpty">Whether to return a value of 0 for empty strings.</param>
+    /// <returns>The 64-bit lookup8 hash of <paramref name="str"/>.</returns>
+    public static ulong Hash(string str, ulong initVal = 0xABCDEF0011223344, bool returnZeroForEmpty = true)
     {
         if (string.IsNullOrEmpty(str) && (str == null || returnZeroForEmpty))
             return 0;
-        byte[] data = Encoding.ASCII.GetBytes(str);
-        var bytesProcessed = (uint)str.Length;
-        var mixVar1 = (ulong)bytesProcessed;
-        var manipulatedPrime = 0x9e3779b97f4a7c13;
-        ulong mixVar2;
-        var manipulatedInit = init;
-        int dataPos = 0;
 
-        if (mixVar1 < 0x18)
+        var stringBytes = Encoding.UTF8.GetBytes(str);
+        ulong len = (ulong)stringBytes.Length;
+        ulong a = initVal;
+        ulong b = initVal;
+        ulong c = 0x9e3779b97f4a7c13; // golden ratio
+
+        var byteSpan = stringBytes.AsSpan();
+
+        // handle most of the key
+        while (len >= 24)
         {
-            manipulatedPrime = 0x9e3779b97f4a7c13;
-            manipulatedInit = init;
+            a += BinaryPrimitives.ReadUInt64LittleEndian(byteSpan);
+            b += BinaryPrimitives.ReadUInt64LittleEndian(byteSpan[8..]);
+            c += BinaryPrimitives.ReadUInt64LittleEndian(byteSpan[16..]);
+            Mix64(ref a, ref b, ref c);
+            byteSpan = byteSpan[24..];
+            len -= 24;
         }
-        else
+
+        // handle the last 23 bytes
+        c += (ulong)stringBytes.Length;
+
+        switch (len)
         {
-            do
-            {
-                var lVar5 = manipulatedInit + data[dataPos + 8] + ((ulong)data[dataPos + 0xf] << 0x38) +
-                            (ulong)data[dataPos + 0xb] * 0x1000000 + ((ulong)data[dataPos + 0xd] << 0x28) +
-                            (ulong)data[dataPos + 9] * 0x100 + (ulong)data[dataPos + 10] * 0x10000 +
-                            ((ulong)data[dataPos + 0xc] << 0x20) + ((ulong)data[dataPos + 0xe] << 0x30);
-                manipulatedPrime = manipulatedPrime + data[dataPos + 0x10] + ((ulong)data[dataPos + 0x17] << 0x38) +
-                                   (ulong)data[dataPos + 0x13] * 0x1000000 + ((ulong)data[dataPos + 0x15] << 0x28) +
-                                   (ulong)data[dataPos + 0x11] * 0x100 + (ulong)data[dataPos + 0x12] * 0x10000 +
-                                   ((ulong)data[dataPos + 0x14] << 0x20) + ((ulong)data[dataPos + 0x16] << 0x30);
-                manipulatedInit = init + data[dataPos + 0] + ((ulong)data[dataPos + 7] << 0x38) +
-                    (ulong)data[dataPos + 3] * 0x1000000 + ((ulong)data[dataPos + 5] << 0x28) +
-                    (ulong)data[dataPos + 1] * 0x100 + (ulong)data[dataPos + 2] * 0x10000 +
-                    ((ulong)data[dataPos + 4] << 0x20) + ((ulong)data[dataPos + 6] << 0x30) - lVar5
-                    - manipulatedPrime ^ manipulatedPrime >> 0x2b;
-                init = lVar5 - manipulatedPrime - manipulatedInit ^ manipulatedInit << 9;
-                mixVar2 = manipulatedPrime - manipulatedInit - init ^ init >> 8;
-                manipulatedInit = manipulatedInit - init - mixVar2 ^ mixVar2 >> 0x26;
-                init = init - mixVar2 - manipulatedInit ^ manipulatedInit << 0x17;
-                mixVar2 = mixVar2 - manipulatedInit - init ^ init >> 5;
-                manipulatedInit = manipulatedInit - init - mixVar2 ^ mixVar2 >> 0x23;
-                manipulatedPrime = init - mixVar2 - manipulatedInit ^ manipulatedInit << 0x31;
-                mixVar2 = mixVar2 - manipulatedInit - manipulatedPrime ^ manipulatedPrime >> 0xb;
-                init = manipulatedInit - manipulatedPrime - mixVar2 ^ mixVar2 >> 0xc;
-                manipulatedInit = manipulatedPrime - mixVar2 - init ^ init << 0x12;
-                manipulatedPrime = mixVar2 - init - manipulatedInit ^ manipulatedInit >> 0x16;
-                dataPos += 0x18;
-                bytesProcessed -= 0x18;
-            } while (0x17 < bytesProcessed);
-        }
-        manipulatedPrime += mixVar1 & 0xffffffff;
-        switch (bytesProcessed)
-        {
-            case 0x17:
-                manipulatedPrime += (ulong)data[dataPos + 0x16] << 0x38;
-                goto case 0x16;
-            case 0x16:
-                manipulatedPrime += (ulong)data[dataPos + 0x15] << 0x30;
-                goto case 0x15;
-            case 0x15:
-                manipulatedPrime += (ulong)data[dataPos + 0x14] << 0x28;
-                goto case 0x14;
-            case 0x14:
-                manipulatedPrime += (ulong)data[dataPos + 0x13] << 0x20;
-                goto case 0x13;
-            case 0x13:
-                manipulatedPrime += (ulong)data[dataPos + 0x12] * 0x1000000;
-                goto case 0x12;
-            case 0x12:
-                manipulatedPrime += (ulong)data[dataPos + 0x11] * 0x10000;
-                goto case 0x11;
-            case 0x11:
-                manipulatedPrime += (ulong)data[dataPos + 0x10] * 0x100;
-                goto case 0x10;
-            case 0x10:
-                manipulatedInit += (ulong)data[dataPos + 0xf] << 0x38;
-                goto case 0xf;
-            case 0xf:
-                manipulatedInit += (ulong)data[dataPos + 0xe] << 0x30;
-                goto case 0xe;
-            case 0xe:
-                manipulatedInit += (ulong)data[dataPos + 0xd] << 0x28;
-                goto case 0xd;
-            case 0xd:
-                manipulatedInit += (ulong)data[dataPos + 0xc] << 0x20;
-                goto case 0xc;
-            case 0xc:
-                manipulatedInit += (ulong)data[dataPos + 0xb] * 0x1000000;
-                goto case 0xb;
-            case 0xb:
-                manipulatedInit += (ulong)data[dataPos + 10] * 0x10000;
-                goto case 0xa;
+            case 23:
+                c += (ulong)byteSpan[22] << 56;
+                goto case 22;
+            case 22:
+                c += (ulong)byteSpan[21] << 48;
+                goto case 21;
+            case 21:
+                c += (ulong)byteSpan[20] << 40;
+                goto case 20;
+            case 20:
+                c += (ulong)byteSpan[19] << 32;
+                goto case 19;
+            case 19:
+                c += (ulong)byteSpan[18] << 24;
+                goto case 18;
+            case 18:
+                c += (ulong)byteSpan[17] << 16;
+                goto case 17;
+            case 17:
+                c += (ulong)byteSpan[16] << 8;
+                goto case 16;
+            // the first byte of c is reserved for the length
+            case 16:
+                b += (ulong)byteSpan[15] << 56;
+                goto case 15;
+            case 15:
+                b += (ulong)byteSpan[14] << 48;
+                goto case 14;
+            case 14:
+                b += (ulong)byteSpan[13] << 40;
+                goto case 13;
+            case 13:
+                b += (ulong)byteSpan[12] << 32;
+                goto case 12;
+            case 12:
+                b += (ulong)byteSpan[11] << 24;
+                goto case 11;
+            case 11:
+                b += (ulong)byteSpan[10] << 16;
+                goto case 10;
             case 10:
-                manipulatedInit += (ulong)data[dataPos + 9] * 0x100;
-                goto case 0x9;
+                b += (ulong)byteSpan[9] << 8;
+                goto case 9;
             case 9:
-                manipulatedInit += data[dataPos + 8];
-                goto case 0x8;
+                b += (ulong)byteSpan[8] << 0;
+                goto case 8;
             case 8:
-                init += (ulong)data[dataPos + 7] << 0x38;
-                goto case 0x7;
+                a += (ulong)byteSpan[7] << 56;
+                goto case 7;
             case 7:
-                init += (ulong)data[dataPos + 6] << 0x30;
-                goto case 0x6;
+                a += (ulong)byteSpan[6] << 48;
+                goto case 6;
             case 6:
-                init += (ulong)data[dataPos + 5] << 0x28;
-                goto case 0x5;
+                a += (ulong)byteSpan[5] << 40;
+                goto case 5;
             case 5:
-                init += (ulong)data[dataPos + 4] << 0x20;
-                goto case 0x4;
+                a += (ulong)byteSpan[4] << 32;
+                goto case 4;
             case 4:
-                init += (ulong)data[dataPos + 3] * 0x1000000;
-                goto case 0x3;
+                a += (ulong)byteSpan[3] << 24;
+                goto case 3;
             case 3:
-                init += (ulong)data[dataPos + 2] * 0x10000;
-                goto case 0x2;
+                a += (ulong)byteSpan[2] << 16;
+                goto case 2;
             case 2:
-                init += (ulong)data[dataPos + 1] * 0x100;
-                goto case 0x1;
+                a += (ulong)byteSpan[1] << 8;
+                goto case 1;
             case 1:
-                init += data[dataPos];
-                goto default;
-            default:
-                mixVar1 = init - manipulatedInit - manipulatedPrime ^ manipulatedPrime >> 0x2b;
-                init = manipulatedInit - manipulatedPrime - mixVar1 ^ mixVar1 << 9;
-                mixVar2 = manipulatedPrime - mixVar1 - init ^ init >> 8;
-                manipulatedInit = mixVar1 - init - mixVar2 ^ mixVar2 >> 0x26;
-                mixVar1 = init - mixVar2 - manipulatedInit ^ manipulatedInit << 0x17;
-                mixVar2 = mixVar2 - manipulatedInit - mixVar1 ^ mixVar1 >> 5;
-                manipulatedInit = manipulatedInit - mixVar1 - mixVar2 ^ mixVar2 >> 0x23;
-                mixVar1 = mixVar1 - mixVar2 - manipulatedInit ^ manipulatedInit << 0x31;
-                mixVar2 = mixVar2 - manipulatedInit - mixVar1 ^ mixVar1 >> 0xb;
-                manipulatedInit = manipulatedInit - mixVar1 - mixVar2 ^ mixVar2 >> 0xc;
-                mixVar1 = mixVar1 - mixVar2 - manipulatedInit ^ manipulatedInit << 0x12;
-                return mixVar2 - manipulatedInit - mixVar1 ^ mixVar1 >> 0x16;
+                a += byteSpan[0];
+                goto case 0;
+            case 0:
+                break;
         }
+
+        Mix64(ref a, ref b, ref c);
+
+        return c;
     }
 }
